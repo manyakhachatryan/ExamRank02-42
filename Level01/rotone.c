f81fd8ce79e9ae2d81cdd678cc847276c354e35c
manyakhachatryan <manykhac@c2r2s5.42yerevan.am>
2022-07-25 21:48:04 +0400

    rotone

---
+++ Level01/rotone.c
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rotone.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: manykhac <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/25 21:22:02 by manykhac          #+#    #+#             */
+/*   Updated: 2022/07/25 21:47:13 by manykhac         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/* Write a program that takes a string and displays it, replacing each of its
+letters by the next one in alphabetical order.
+
+'z' becomes 'a' and 'Z' becomes 'A'. Case remains unaffected.
+
+The output will be followed by a \n.
+
+If the number of arguments is not 1, the program displays \n.
+
+Example:
+
+$>./rotone "abc"
+bcd
+$>./rotone "Les stagiaires du staff ne sentent pas toujours tres bon." | cat -e
+Mft tubhjbjsft ev tubgg of tfoufou qbt upvkpvst usft cpo.$
+$>./rotone "AkjhZ zLKIJz , 23y " | cat -e
+BlkiA aMLJKa , 23z $
+$>./rotone | cat -e
+$
+$>
+$>./rotone "" | cat -e
+$
+$>  */
+
+#include <unistd.h>
+
+int main(int argc, char **argv)
+{
+	int i;
+	
+	i = 0;
+	if (argc == 2)
+	{
+		while(argv[1][i] != '\0')
+		{ 
+			if (argv[1][i] == 90 || argv[1][i] == 122)
+			{
+				argv[1][i] = argv[1][i] - 25;
+			}
+			else if ((argv[1][i] >= 65 && argv[1][i] <= 89) || (argv[1][i] >= 97&& argv[1][i] <= 121)) 
+			{
+				argv[1][i]  = argv[1][i] + 1;
+			}
+			write(1, &argv[1][i], 1);
+			i++;
+		}
+	}
+	write (1, "\n", 1);
+	return 0;
+}
