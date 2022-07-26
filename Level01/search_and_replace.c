32ad51d915786ae79b34ffbbda375da3c7a78085
manyakhachatryan <manykhac@c2r3s3.42yerevan.am>
2022-07-26 19:48:47 +0400

    search_and_replace

---
+++ Level01/search_and_replace.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   search_and_replace.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: manykhac <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/26 19:33:53 by manykhac          #+#    #+#             */
+/*   Updated: 2022/07/26 19:47:11 by manykhac         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/* Write a program called search_and_replace that takes 3 arguments, the first
+arguments is a string in which to replace a letter (2nd argument) by
+another one (3rd argument).
+
+If the number of arguments is not 3, just display a newline.
+
+If the second argument is not contained in the first one (the string)
+then the program simply rewrites the string followed by a newline.
+
+Examples:
+$>./search_and_replace "Papache est un sabre" "a" "o"
+Popoche est un sobre
+$>./search_and_replace "zaz" "art" "zul" | cat -e
+$
+$>./search_and_replace "zaz" "r" "u" | cat -e
+zaz$
+$>./search_and_replace "jacob" "a" "b" "c" "e" | cat -e
+$
+$>./search_and_replace "ZoZ eT Dovid oiME le METol." "o" "a" | cat -e
+ZaZ eT David aiME le METal.$
+$>./search_and_replace "wNcOre Un ExEmPle Pas Facilw a Ecrirw " "w" "e" | cat -e
+eNcOre Un ExEmPle Pas Facile a Ecrire $   */
+
+
+#include <unistd.h>
+
+int main(int argc, char **argv)
+{
+    int i;
+
+    i = 0;
+    if(argc == 4 && !argv[2][1] & !argv[3][1])
+    {
+        while(argv[1][i]!='\0')
+        {
+            if (argv[1][i]==argv[2][0] )
+            {
+                argv[1][i] = argv[3][0];
+            }
+            write (1, &argv[1][i], 1);
+            i++;
+        }
+    }
+write (1, "\n", 1);
+}
