4461711297cf4d04fe65ba7301ca4f9b3202eb87
manyakhachatryan <manykhac@c2r14s3.42yerevan.am>
2022-07-28 19:33:19 +0400

    camel_to_snake

---
+++ Level02/camel_to_snake.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   camel_to_snake.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: manykhac <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/28 15:14:57 by manykhac          #+#    #+#             */
+/*   Updated: 2022/07/28 19:31:26 by manykhac         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*Assignment name  : camel_to_snake
+Expected files   : camel_to_snake.c
+Allowed functions: malloc, free, realloc, write
+--------------------------------------------------------------------------------
+
+Write a program that takes a single string in lowerCamelCase format
+and converts it into a string in snake_case format.
+
+A lowerCamelCase string is a string where each word begins with a capital letter
+except for the first one.
+
+A snake_case string is a string where each word is in lower case, separated by
+an underscore "_".
+
+Examples:
+$>./camel_to_snake "hereIsACamelCaseWord"
+here_is_a_camel_case_word
+$>./camel_to_snake "helloWorld" | cat -e
+hello_world$
+$>./camel_to_snake | cat -e
+$  */
+
+#include <unistd.h>
+
+int main(int argc, char **argv)
+{
+	int i;
+
+	i = 0;
+	if (argc == 2)
+	{
+		while(argv[1][i])
+		{
+			if(argv[1][i] >= 65 && argv[1][i] <= 90)
+			{
+				argv[1][i] = argv[1][i] + 32;
+				write (1, "_", 1);
+			}
+			write(1, &argv[1][i], 1);
+			i++;
+		}
+	}
+	write (1, "\n", 1);
+}
