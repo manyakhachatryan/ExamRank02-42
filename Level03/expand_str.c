66025e55575f344fc755938bbfdb6970d2d7c8ec
manyakhachatryan <manykhac@c2r8s4.42yerevan.am>
2022-08-02 18:33:34 +0400

    expend_str.c

---
+++ Level03/expand_str.c
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   expand_str.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: manykhac <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/02 18:21:35 by manykhac          #+#    #+#             */
+/*   Updated: 2022/08/02 18:33:09 by manykhac         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/* Assignment name  : expand_str
+Expected files   : expand_str.c
+Allowed functions: write
+--------------------------------------------------------------------------------
+
+Write a program that takes a string and displays it with exactly three spaces
+between each word, with no spaces or tabs either at the beginning or the end,
+followed by a newline.
+
+A word is a section of string delimited either by spaces/tabs, or by the
+start/end of the string.
+
+If the number of parameters is not 1, or if there are no words, simply display
+a newline.
+
+Examples:
+
+$> ./expand_str "See? It's easy to print the same thing" | cat -e
+See?   It's   easy   to   print   the   same   thing$
+$> ./expand_str " this        time it      will     be    more complex  " | cat -e
+this   time   it   will   be   more   complex$
+$> ./expand_str "No S*** Sherlock..." "nAw S*** ShErLaWQ..." | cat -e
+$
+$> ./expand_str "" | cat -e
+$
+$>    */
+
+#include <unistd.h>
+
+int main(int argc, char **argv)
+{
+	int i = 0;
+	if (argc == 2)
+	{
+		while(argv[1][i])
+		{
+			if (argv[1][i] == ' ' || argv[1][i] == '\n')
+			{
+				i++;
+			}
+			else if ((argv[1][i] != ' ' && argv[1][i] != '\t') && (argv[1][i+1] == ' ' || argv[1][i] == '\t'))
+			{
+				write (1, &argv[1][i], 1);
+				write (1, "   ", 3);
+				i++;
+			}
+			else if (argv[1][i] != ' ' && argv[1][i] != '\t' )
+			{
+				write (1, &argv[1][i], 1);
+				i++;
+			}
+		}
+	}
+	write (1, "\n", 1);
+}
