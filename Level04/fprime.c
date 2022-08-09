29b3611de3f022c7a1c005c8d6c3b0317acc755f
manyakhachatryan <manykhac@c2r2s2.42yerevan.am>
2022-08-09 14:02:18 +0400

    fprime.c

---
+++ Level04/fprime.c
@@ -0,0 +1,76 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fprime.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: manykhac <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/09 13:23:38 by manykhac          #+#    #+#             */
+/*   Updated: 2022/08/09 14:01:52 by manykhac         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/* Assignment name  : fprime
+Expected files   : fprime.c
+Allowed functions: printf, atoi
+--------------------------------------------------------------------------------
+
+Write a program that takes a positive int and displays its prime factors on the
+standard output, followed by a newline.
+
+Factors must be displayed in ascending order and separated by '*', so that
+the expression in the output gives the right result.
+
+If the number of parameters is not 1, simply display a newline.
+
+The input, when there is one, will be valid.
+
+Examples:
+
+$> ./fprime 225225 | cat -e
+3*3*5*5*7*11*13$
+$> ./fprime 8333325 | cat -e
+3*3*5*5*7*11*13*37$
+$> ./fprime 9539 | cat -e
+9539$
+$> ./fprime 804577 | cat -e
+804577$
+$> ./fprime 42 | cat -e
+2*3*7$
+$> ./fprime 1 | cat -e
+1$
+$> ./fprime | cat -e
+$
+$> ./fprime 42 21 | cat -e
+$    */
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+	if (argc==2)
+	{
+		int i =2;
+		int num;
+		num = atoi(argv[1]);
+		if (num ==1)
+			printf("1");
+		while(i <= num)
+		{
+			if (num % i == 0)
+			{
+				printf("%d", i);
+				if (num == i)
+					break;
+				printf("*");
+				num = num / i;
+				i = 2;
+			}
+			i++;
+		}
+	}
+	printf("\n");
+}
